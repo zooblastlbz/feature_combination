e17a4d0b6eab18d935003c6a943735e70b547e9e
libozhou <libozhou@kuaishou.com>
2025-11-21 16:12:29 +0800

    change save pipeline

--- save_pipeline.sh
+++ save_pipeline.sh
@@ -1,4 +1,6 @@
 python utils/save_pipeline.py \
     --checkpoint /ytech_m2v5_hdd/workspace/kling_mm/libozhou/feature_combination/output/256-AdaFuseDiT-timewise/30000 \
     --type adafusedit \
-    --trainer deepspeed
+    --trainer deepspeed \
+    --vae /ytech_m2v5_hdd/workspace/kling_mm/Models/Lumina-Image-2.0/vae \
+    --scheduler /ytech_m2v5_hdd/workspace/kling_mm/Models/Lumina-Image-2.0/scheduler/ \
