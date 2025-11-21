84ea01eb1733749379d93b57076ea38a85d087cd
libozhou <libozhou@kuaishou.com>
2025-11-21 15:51:02 +0800

    add save_pipeline.sh

---
+++ save_pipeline.sh
@@ -0,0 +1,4 @@
+python utils/save_pipeline.py \
+    --checkpoint /ytech_m2v5_hdd/workspace/kling_mm/libozhou/feature_combination/output/256-AdaFuseDiT-timewise/30000 \
+    --type adafusedit \
+    --trainer deepspeed
