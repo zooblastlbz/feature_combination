c7bd59963b5f2ba4ebd917bc93cd1ec5232b1a4f
libozhou <libozhou@kuaishou.com>
2025-12-12 16:10:20 +0800

    repair save pipeline

--- save_pipeline.sh
+++ save_pipeline.sh
@@ -1,8 +1,6 @@
 python utils/save_pipeline.py \
-    --checkpoint /ytech_m2v5_hdd/workspace/kling_mm/libozhou/feature_combination/output/256-AdaFuseDiT-timewise/ \
+    --checkpoint /ytech_m2v8_hdd/workspace/kling_mm/libozhou/feature_combination/output/256-AdaFuseDiT-timewise-LNzero/checkpoint-370000 \
     --type adafusedit \
-    --trainer deepspeed \
     --vae /ytech_m2v5_hdd/workspace/kling_mm/Models/Lumina-Image-2.0/vae \
     --scheduler /ytech_m2v5_hdd/workspace/kling_mm/Models/Lumina-Image-2.0/scheduler/ \
-    --tag 35000 \
     --llm_path /ytech_m2v5_hdd/workspace/kling_mm/Models/Qwen3-VL-4B-Instruct/ \
