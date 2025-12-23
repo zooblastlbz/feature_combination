412c4ad963693cf231daec9c4e73243482983014
libozhou <libozhou@kuaishou.com>
2025-12-23 16:51:34 +0800

    add genaibench

--- gen.sh
+++ gen.sh
@@ -1,1 +1,4 @@
-accelerate launch evaluation/sample_geneval.py evaluation/geneval.yaml
+#accelerate launch evaluation/sample_geneval.py evaluation/geneval.yaml
+
+
+accelerate launch evaluation/sample_genaibench.py evaluation/genaibench.yaml
